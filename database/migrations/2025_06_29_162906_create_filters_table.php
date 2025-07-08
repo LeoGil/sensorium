a0bb4d120cab82562f3b5db3b9650cdf6af9bed1
Leonardo Gil <leogilc@gmail.com>
2025-07-08 11:04:03 -0300

    feat: add brand column to filters table migration

--- database/migrations/2025_06_29_162906_create_filters_table.php
+++ database/migrations/2025_06_29_162906_create_filters_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('filters', function (Blueprint $table) {
             $table->id();
             $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->string('brand');
             $table->string('name');
             $table->text('description')->nullable();
             $table->timestamps();
